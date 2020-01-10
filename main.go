1251310a2b4f5cfba91b209aa932e2a8b5763d85
Adib Aulia Rahmansyah <adibaulia@gmail.com>
2020-01-10 18:09:48 +0700

    check the fun hehe

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/dghubble/go-twitter/twitter"
@@ -12,29 +12,59 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+var client *twitter.Client
+var count int
+
+type Request struct {
+	Tweet string `json:"tweet,omitempty"`
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	e := echo.New()
-	e.GET("/", simple)
+	tweet := e.Group("/tweet")
+	tweet.POST("/create", createTweet)
 	e.Logger.Fatal(e.Start(":" + port))
-
+	doEvery(20*time.Second, forFun)
 }
 
-func simple(c echo.Context) error {
+func doEvery(d time.Duration, f func(time.Time)) {
+	for x := range time.Tick(d) {
+		f(x)
+	}
+}
+
+func init() {
 	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET_KEY"))
 	token := oauth1.NewToken(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_SECRET"))
 	httpClient := config.Client(oauth1.NoContext, token)
-
 	// Twitter client
-	client := twitter.NewClient(httpClient)
+	client = twitter.NewClient(httpClient)
+}
 
+func createTweet(c echo.Context) error {
+	body := new(Request)
+	if err := c.Bind(&body); err != nil {
+		log.Print("ERROR", err)
+		return err
+	}
 	// Send a Tweet
-	_, resp, err := client.Statuses.Update(fmt.Sprintf("just setting up my twttr %s", time.Now()), nil)
+	tweet, resp, err := client.Statuses.Update(body.Tweet, nil)
 	if err != nil {
 		log.Print(err)
 		return c.JSON(http.StatusBadRequest, http.Response{Status: err.Error()})
 	}
-	log.Print("OKOKOKOKOKO")
 	log.Print(resp)
-	return c.JSON(http.StatusOK, http.Response{Status: "Tweeted"})
+	return c.JSON(http.StatusOK, http.Response{Status: tweet.Text})
+}
+
+func forFun(t time.Time) {
+
+	tweet := `NGELU NDASKU ` + `(` + strconv.Itoa(count) + `)`
+	count++
+	_, resp, err := client.Statuses.Update(tweet, nil)
+	if err != nil {
+		log.Print(err)
+	}
+	log.Print(resp)
 }
