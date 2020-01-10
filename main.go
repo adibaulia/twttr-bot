1e0079c4a5af774ccbac9ed077261f82b843d3dc
Adib Aulia Rahmansyah <adibaulia@gmail.com>
2020-01-10 14:42:08 +0700

    change procfile

--- main.go
+++ main.go
@@ -1,29 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net/http"
 	"os"
+	"time"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
+	e := echo.New()
+	e.POST("/", simple)
+	e.Logger.Fatal(e.Start(":3003"))
+
+}
+
+func simple(c echo.Context) error {
+	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET_KEY"))
+	token := oauth1.NewToken(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_SECRET"))
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	// Send a Tweet
+	_, resp, err := client.Statuses.Update(fmt.Sprintf("just setting up my twttr %s", time.Now()), nil)
+	if err != nil {
+		log.Print(err)
 	}
-
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.Run(":" + port)
+	log.Print("OKOKOKOKOKO")
+	log.Print(resp)
+	return nil
 }
