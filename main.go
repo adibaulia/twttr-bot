49fe34a6986f58595bbe9d3ffeec5052204702fe
Adib Aulia Rahmansyah <adibaulia@gmail.com>
2020-01-13 11:28:49 +0700

    add CRC webhooks

--- main.go
+++ main.go
@@ -6,15 +6,23 @@ import (
 	"os"
 	"time"
 
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 	"github.com/labstack/echo/v4"
 )
 
-var client *twitter.Client
+var (
+	client       *twitter.Client
+	CONSUMER_KEY = os.Getenv("CONSUMER_SECRET_KEY")
+)
 
 type Request struct {
-	Tweet string `json:"tweet,omitempty"`
+	Tweet     string `json:"tweet,omitempty"`
+	Crc_token string `json:"crc_token,omitempty" form:"crc_token" query:"crc_token"`
 }
 
 func main() {
@@ -23,6 +31,7 @@ func main() {
 	tweet := e.Group("/tweet")
 	tweet.POST("/create", createTweet)
 	e.POST("/dev/webhooks", webhookEvent)
+	e.GET("/dev/webhooks", CRC)
 	e.Logger.Fatal(e.Start(":" + port))
 }
 
@@ -44,6 +53,28 @@ func init() {
 	for {
 		doEvery(2*time.Second, forFun)
 	}
+}
+
+func CRC(c echo.Context) error {
+	body := new(Request)
+	if err := c.Bind(&body); err != nil {
+		log.Print("ERROR", err)
+		return err
+	}
+
+	secret := []byte(CONSUMER_KEY)
+	message := []byte(body.Crc_token)
+
+	hash := hmac.New(sha256.New, secret)
+	hash.Write(message)
+
+	// to base64
+	token := base64.StdEncoding.EncodeToString(hash.Sum(nil))
+
+	resp := map[string]string{
+		"response_token": token,
+	}
+	return c.JSON(http.StatusOK, resp)
 }
 
 func createTweet(c echo.Context) error {
